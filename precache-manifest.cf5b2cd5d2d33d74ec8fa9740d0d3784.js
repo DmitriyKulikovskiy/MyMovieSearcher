656fd7e30a6c0814d4b36276b3ffacc807dc4698
DmitriyKulikovskiy <59229226+DmitriyKulikovskiy@users.noreply.github.com>
2020-05-02 14:07:26 +0300

    Updates

---
+++ precache-manifest.cf5b2cd5d2d33d74ec8fa9740d0d3784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b9f6ae306a70cf589cef85b08c81a9",
+    "url": "/MyMovieSearcher/index.html"
+  },
+  {
+    "revision": "a4e8d9c9bafbbef4cf97",
+    "url": "/MyMovieSearcher/static/css/main.6b31e48e.chunk.css"
+  },
+  {
+    "revision": "e09f0b8c2b7831dcec11",
+    "url": "/MyMovieSearcher/static/js/2.f3a9fe58.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MyMovieSearcher/static/js/2.f3a9fe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4e8d9c9bafbbef4cf97",
+    "url": "/MyMovieSearcher/static/js/main.d33addbf.chunk.js"
+  },
+  {
+    "revision": "8dca85b41767543b2aa0",
+    "url": "/MyMovieSearcher/static/js/runtime-main.e67da9aa.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MyMovieSearcher/precache-manifest.cf5b2cd5d2d33d74ec8fa9740d0d3784.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyMovieSearcher/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
